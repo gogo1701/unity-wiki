b79e1c07702f38419a679dad2631edbe2eb5d068
gogo1701 <gogo170109@gmail.com>
2017-10-29 14:38:45 +0200

    Add skeleton

---
+++ UnityWiki/UnityWiki.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UnityWiki.Web.Startup))]
+namespace UnityWiki.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
